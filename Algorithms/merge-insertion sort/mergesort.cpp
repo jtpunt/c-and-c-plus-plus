9f59fd7f40c3686e6bbb1b115d0e3935b0c64e3f
jtpunt <perryjon@oregonstate.edu>
2018-03-19 22:03:23 -0500

    Update mergesort.cpp

--- Algorithms/merge-insertion sort/mergesort.cpp
+++ Algorithms/merge-insertion sort/mergesort.cpp
@@ -19,31 +19,25 @@ using std::endl;
 // This function receives the address to a vector so that the function
 // can manipulate/merge the two halves in sorted order.
 void merge(vector <int> &v, int low, int high, int mid){
-	int i = low,
-		j = mid + 1;
+	int i = low, j = mid + 1;
 	vector <int> temp;
 
 	// Merge the two halves into temp
 	while (i <= mid && j <= high){
-		if (v[i] < v[j]){
+		if (v[i] < v[j])
 			temp.push_back(v[i++]);
-		}
-		else{
+		else
 			temp.push_back(v[j++]);
-		}
 	}
 	// Push all the remaining values from i to mid into temp
-	while (i <= mid){
+	while (i <= mid)
 		temp.push_back(v[i++]);
-	}
 	// Push all the remaining values from j to high into temp
-	while (j <= high){
+	while (j <= high)
 		temp.push_back(v[j++]);
-	}
 	// Assign the sorted data stored from temp to v.
-	for (i = low; i <= high; i++){
+	for (i = low; i <= high; i++)
 		v[i] = temp[i-low];
-	}
 }
 // This function receives the address to a vector so that the function
 // can recursively split the vector in two halves until both halves
